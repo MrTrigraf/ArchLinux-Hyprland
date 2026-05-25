ed10e5e04be5b27c3f4681cd57583b736d8e288a
Barmaley <178800232+MrTrigraf@users.noreply.github.com>
2026-05-25 18:40:42 +0400

    ignore fish_variables

---
+++ kanagawa-paper/hypr/hyprland.lua
@@ -0,0 +1,74 @@
+-----------------
+---- MODULES ----
+-----------------
+
+require("modules.keybindings")
+require("modules.env")
+require("modules.visual")
+require("modules.misc")
+require("modules.windowrules")
+
+------------------
+---- MONITORS ----
+------------------
+
+--monitor.Deskstop
+--hl.monitor({
+--   output   = "HDMI-A-1",
+--    mode     = "1920x1080@144",
+--    position = "auto",
+--    scale    = "1.0",
+--})
+
+--monitor.Laptop
+hl.monitor({
+    output   = "eDP-1",
+    mode     = "2160x1440@60",
+    position = "auto",
+    scale    = "1.0",
+})
+
+---------------------
+---- MY PROGRAMS ----
+---------------------
+
+local terminal    = "kitty"
+local fileManager = "nautilus"
+local browser     = "firefox"
+local menu        = "rofi -show drun"
+
+-------------------
+---- AUTOSTART ----
+-------------------
+
+-- hl.on("hyprland.start", function ()
+--   hl.exec_cmd(terminal)
+--   hl.exec_cmd("nm-applet")
+--   hl.exec_cmd("waybar & hyprpaper & firefox")
+-- end)
+
+---------------
+---- LAYOT ----
+---------------
+
+hl.config({
+    general = {
+    layout = "dwindle"
+    },
+
+    dwindle = {
+        preserve_split = true, -- You probably want this
+    },
+})
+
+hl.config({
+    master = {
+        new_status = "master",
+    },
+})
+
+hl.config({
+    scrolling = {
+        fullscreen_on_one_column = true,
+    },
+})
---
+++ kanagawa-paper/hypr/keybindings.lua
@@ -0,0 +1,35 @@
+---------------
+---- INPUT ----
+---------------
+
+hl.config({
+    input = {
+        kb_layout  = "us,ru",
+        kb_variant = "",
+        kb_model   = "",
+        kb_options = "grp:alt_shift_toggle",
+        kb_rules   = "",
+
+        follow_mouse = 1,
+
+        sensitivity = 0, -- -1.0 - 1.0, 0 means no modification.
+
+        touchpad = {
+            natural_scroll = false,
+        },
+    },
+})
+
+hl.gesture({
+    fingers = 3,
+    direction = "horizontal",
+    action = "workspace"
+})
+
+---------------------
+---- KEYBINDINGS ----
+---------------------
+
+local mainMod = "SUPER"
+
+
