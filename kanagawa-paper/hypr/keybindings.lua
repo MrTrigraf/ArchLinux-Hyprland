c8d4404b335247f3594ec7f79a12072138e72ad6
Barmaley <178800232+MrTrigraf@users.noreply.github.com>
2026-05-28 20:01:26 +0400

    hyprlock config

--- kanagawa-paper/hypr/keybindings.lua
+++ kanagawa-paper/hypr/keybindings.lua
@@ -48,6 +48,7 @@ hl.bind(mainMod .. " + N",          hl.dsp.exec_cmd(terminal .. " " .. ide))
 hl.bind(mainMod .. " + E",          hl.dsp.exec_cmd(fileManager))
 hl.bind(mainMod .. " + B",          hl.dsp.exec_cmd(browser))
 hl.bind(mainMod .. " + R",          hl.dsp.exec_cmd(menu))
+hl.bind(mainMod .. " + SHIFT + L",  hl.dsp.exec_cmd("hyprlock --config ~/.config/hypr/kanagawa-paper/hypr/hyprlock.conf"))
 ---- utils ----
 hl.bind("Print",                    hl.dsp.exec_cmd("hyprshot --mode region --clipboard-only")) --PrintScrin в буфер
 hl.bind("SHIFT + Print",            hl.dsp.exec_cmd("hyprshot --mode region --output-folder ~/Изображения")) --PrintScrin на диск
